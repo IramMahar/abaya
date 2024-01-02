258dd4fbd8bfd5500af11fd31fa52d3b42727eae
AboubakarArisar <aboubakar.bscsf22@iba-suk.edu.pk>
2024-01-02 16:20:54 +0500

    this is my first version

---
+++ app/src/main/java/com/example/abayadesign/regestration.kt
@@ -0,0 +1,11 @@
+package com.example.abayadesign
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class regestration : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_regestration)
+    }
+}
